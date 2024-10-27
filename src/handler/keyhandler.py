5be02f6ee2cddb9da1d917889054857b7edbed1a
Emelianov Artem <yaz008.yaz008@yandex.ru>
2024-10-27 18:47:45 +0300

    Add template command

--- src/handler/keyhandler.py
+++ src/handler/keyhandler.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass, field
 from pynput.keyboard import Key, KeyCode
 from j2pipeline import Prompt
 from clipboard import Clipboard
+from os import listdir
 from root import PROJECT_ROOT
 
 AnyKey = Key | KeyCode
@@ -23,17 +24,17 @@ class KeyHandler:
             case [self.toggle]: self.__on_toggle()
             case _:
                 if self.__is_activated:
-                    self.__on_pressed(pressed_key)
+                    self.__extend_message(pressed_key)
 
     def __on_toggle(self) -> None:
         if self.__is_activated and self.__message != str():
-            with Clipboard() as clipboard:
-                clipboard.data = self.__prompt(text=self.__message)
-                self.__message = str()
+            match self.__message[0]:
+                case '/': self.__execute_command(command=self.__message[1:])
+                case _: self.__set_clipboard()
+            self.__message = str()
         self.__is_activated = not self.__is_activated
 
-    def __on_pressed(self, pressed_key: AnyKey) -> None:
-        if self.__message[-2:] == '``': self.__message = str()
+    def __extend_message(self, pressed_key: AnyKey) -> None:
         if pressed_key == Key.backspace:
             self.__message = self.__message[:-1]
         if isinstance(pressed_key, KeyCode):
@@ -42,3 +43,14 @@ class KeyHandler:
             Key.enter: '\n',
             Key.space: ' '
         }.get(pressed_key, str())
+
+    def __execute_command(self, command: str) -> None:
+        match command.split(sep=' '):
+            case ['template' | 't', filename]:
+                template_path: str = f'{PROJECT_ROOT}\\prompts\\{filename}.j2'
+                if f'{filename}.j2' in listdir(f'{PROJECT_ROOT}\\prompts'):
+                    self.__prompt = Prompt[str](path=template_path)
+                 
+    def __set_clipboard(self) -> None:
+        with Clipboard() as clipboard:
+            clipboard.data = self.__prompt(prompt=self.__message)
